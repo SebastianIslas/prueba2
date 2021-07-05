e6f94bab0e83a0bf5fb514ea9d70dd7a7b9ff013
SebastianIslas <78067110+SebastianIslas@users.noreply.github.com>
2021-07-05 16:56:19 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+//const http = require('http');
+//require('dotenv').config();
+//const{ fechaCool } = require('./modules/library')
+
+//function request(req, res){
+    //const resultado = fechaCool();
+    //res.write(`La fecha formateada con Moment quedo asi ${resultado}`);
+    //res.end();
+//}
+//const server = http.createServer(request);
+
+//server.listen(process.env.PORT, process.env.HOST, () => {
+    //console.log(`Servidor iniciado en https://${process.env.HOST}:${process.env.PORT}`);
+//})
